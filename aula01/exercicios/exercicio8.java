535445a161dc0a920fd06a7bfd33a7f042de1738
luiza <97320223+aaziul@users.noreply.github.com>
2023-10-16 12:53:29 -0300

    Atualizar o exercicio8.java

--- aula01/exercicios/exercicio8.java
+++ aula01/exercicios/exercicio8.java
@@ -6,6 +6,8 @@ public class exercicio8 {
     public static void main(String[] args) {
     System.out.println("EXERCÍCIO 08: Criar um algoritmo para definir as filas prioritárias. Seu programa deve exibir um menu de opções, perguntando se a pessoa é Gestante, Idosa, PCD ou Nenhuma das alternativas. Se ela for PCD, Idosa ou Gestante, tem direito a fila prioritária. Se não, não tem direito.");
 
+    Scanner ent = new Scanner(System.in);
+
     int sim = 1;
     int nao = 0;
 
